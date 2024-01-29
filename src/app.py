45fa0727b383e7b4d149d2960deb83b66c5839e5
Kaito Shigyo / 執行凱斗 <shigyo.kaito.437@s.kyushu-u.ac.jp>
2024-01-29 16:32:15 +0900

    change db from sqlite to mysql

--- src/app.py
+++ src/app.py
@@ -6,9 +6,8 @@ The database setup utilizes SQLAlchemy and SQLModel.
 The session setup involves initializing API client services for authentication, account management, and history.
 The application setup involves creating an instance of MultiPageApp.
 """
-from pathlib import Path
-
 import sqlalchemy
+import sqlalchemy_utils
 import sqlmodel
 import streamlit as st
 
@@ -16,18 +15,22 @@ import page
 import session
 
 
-def initialize_database(db_dir_path: str, sqlite_url: str) -> sqlalchemy.Engine:
+def initialize_database(db_user: str, db_password: str, db_host: str, db_port: str, db_name: str) -> sqlalchemy.Engine:
     """Initialize database. This function includes side effects.
 
     Args:
-        db_dir_path (str): The path to the directory where the database file will be stored.
-        sqlite_url (str): The URL to the SQLite database.
-
+        db_user (str): Database user.
+        db_password (str): Database password.
+        db_host (str): Database host.
+        db_port (str): Database port.
+        db_name (str): Database name.
     Returns:
         sqlalchemy.Engine: Database engine.
     """
-    Path(__file__).parent.parent.joinpath(db_dir_path).mkdir(exist_ok=True)
-    engine = sqlmodel.create_engine(sqlite_url)
+    db_url = f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+    engine = sqlmodel.create_engine(db_url)
+    if not sqlalchemy_utils.database_exists(engine.url):
+        sqlalchemy_utils.create_database(engine.url)
     sqlmodel.SQLModel.metadata.create_all(engine)
     return engine
 
@@ -38,7 +41,7 @@ def initialize_app() -> page.PageController:
     This function initializes the database and the session manager. It also sets the page configuration.
     """
     session_manager = session.SessionManager()
-    engine = initialize_database("db", "sqlite:///db/pairs.db")
+    engine = initialize_database(**st.secrets.db_credentials)
     return page.PageController(session_manager, engine)
 
 
